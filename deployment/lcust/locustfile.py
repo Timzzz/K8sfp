b990aff42ecc113b5df6d96e93120b2e4ac853b5
zwietatm <zwietatm@gspc45.informatik.uni-stuttgart.de>
2017-04-08 19:14:50 +0200

    locust python

--- deployment/lcust/locustfile.py
+++ deployment/lcust/locustfile.py
@@ -7,11 +7,14 @@ from influxdb import InfluxDBClient
 import os
 from requests.exceptions import ConnectionError
 import urllib2
+import schedule
+import time
 
 class MyTaskSet(TaskSet):
     
     def on_start(self):
         self.user_id = randint(1,999999999)
+	schedule.every(1).seconds.do(job)
 
     @task(1)
     def view(self):
@@ -112,33 +115,25 @@ class MyTaskSet(TaskSet):
 	curr_requests = curr_requests + 1
 	if(response.status_code != 200):
 		curr_fails = curr_fails + 1
-        json_body = [
-                {
-                    "measurement": "test_results",
-                    "tags": {
-                        "status_code": str(response.status_code),
-                        "reason": response.reason,
-                        "url": response.request.url,
-                        "path_url": response.request.path_url,
-                        "method": response.request.method,
-                        "body": response.request.body,
-                        "curr_requests": curr_requests,
-                        "curr_fails": curr_fails
-                        },
-                    "fields": {
-                        "status_code": str(response.status_code),
-                        "reason": response.reason,
-                        "elapsed": response.elapsed.total_seconds(),
-                        "url": response.request.url,
-                        "path_url": response.request.path_url,
-                        "method": response.request.method,
-                        "body": response.request.body,
-                        "curr_requests": curr_requests,
-                        "curr_fails": curr_fails
-                        }
-                    }
-                ]
-        InfluxDBWriter.write(json_body)
+	schedule.run_pending()
+        
+
+	def scheduled_write(self):
+		json_body = [
+		{
+			"measurement": "test_results",
+			"tags": {
+			"curr_requests": curr_requests,
+			"curr_fails": curr_fails
+		},
+			"fields": {
+       			"curr_requests": curr_requests,
+			"curr_fails": curr_fails
+			}
+		}
+		]
+		InfluxDBWriter.write(json_body)
+
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
