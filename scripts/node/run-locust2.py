0b90d0af65d7a8e1782623269edd3fb13ebc4129
Timzzz <tim@zwietasch.de>
2017-04-21 16:09:37 +0200

    locustfile

--- scripts/node/run-locust2.py
+++ scripts/node/run-locust2.py
@@ -13,14 +13,18 @@ locust_master_port = "31050"
 # (num_users, duration in minutes)
 profile = [(3,3),(5,3),(10,3),(20,3),(50,3),(100,3),(150,3),(200,3),(250,3),(300,3),(350,3),(400,3),(500,3),(600,3)]
 
-def create_profile(count, maxUsers):
-    intervals = []
+def create_linear_profile(count, maxUsers, execTime):
+    intervals = [(1,1)]
     max_interval = 0
-    for p in range(count):
-        max_interval += random.randint(0, int(maxUsers / int(count/2)))
-        intervals.append(new_interval)
+    for i in range(count):
+        max_interval += random.randint(1, int(maxUsers / int(count/(2))))
+        intervals.append((max_interval,execTime))
+    if(max_interval > maxUsers):
+        for i in range(count):
+            intervals[i] = (max(0, intervals[i][0] - int((max_interval-maxUsers)/((count+1)/(i+1)))), execTime)
     print("profile: ", intervals)
-    
+    return intervals
+        
 def change_workload(num_users):
     if (num_users <= 0):
         r = requests.get("http://" + k8s_node_ip + ":" + locust_master_port + "/stop")
@@ -28,11 +32,13 @@ def change_workload(num_users):
         payload = {'locust_count': num_users, 'hatch_rate': num_users}
         r = requests.post("http://" + k8s_node_ip + ":" + locust_master_port + "/swarm", data=payload)
 
-for p in profile:
-    print(time.strftime("%Y-%m-%d %H:%M:%S"), int(time.time()), "Users=", p[0], "Duration=", p[1], "min")
-    change_workload(p[0])
-    time.sleep(p[1]*60)
+intervals = create_linear_profile(15, 600, 3)
+
+#for p in profile:
+#    print(time.strftime("%Y-%m-%d %H:%M:%S"), int(time.time()), "Users=", p[0], "Duration=", p[1], "min")
+#    change_workload(p[0])
+#    time.sleep(p[1]*60)
 
 # Stop at the end
 #change_workload(0)
-create_profile(14, 600)
+
---
+++ scripts/node/run-workflow1.sh
@@ -0,0 +1,4 @@
+
+python run-locust2.py > run-locust2.log
+
+
